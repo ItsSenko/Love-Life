8d449fc5df7712d586a72f9578f07aa638e93d12
SafeBuckle <69005869+SafeBuckleDev@users.noreply.github.com>
2020-08-28 17:56:27 +0200

    Money system
    pressing + will increase the money amount
    
    pressing - will decrease the money amount
    
    pressing r will reset the money to its default value (1000 yen)
    
    pressing q will save the current money value
    
    pressing l will load the last saved money value if there is nothing saved it will be 0 yen.
    
    this system is used for testing this can be easilty expanded to buying/getting money.

---
+++ Love Life (1)/Assets/counters/money.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class money : MonoBehaviour
+{
+    public string moneydisplay;
+    public float Money = 1000f;
+    public TextMeshProUGUI text;
+
+    public void Update()
+    {
+        moneydisplay = "¥ " + Money.ToString();
+        text.SetText(moneydisplay);
+
+        if (Input.GetKeyDown(KeyCode.Equals))
+        {
+            Money += 50f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            Money -= 50f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            PlayerPrefs.SetFloat("Money", Money);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Money = PlayerPrefs.GetFloat("Money");
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Money = 1000f;
+        }
+    }
+}
