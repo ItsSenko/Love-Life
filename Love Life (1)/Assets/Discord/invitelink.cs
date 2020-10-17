b89e0fb2ff86052b211ed529560b9e0b749f6f1e
SafeBuckle <69005869+SafeBuckleDev@users.noreply.github.com>
2020-10-17 19:01:30 +0200

    added discord invite link

---
+++ Love Life (1)/Assets/Discord/invitelink.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class invitelink : MonoBehaviour
+{
+    public void Click()
+    {
+        Application.OpenURL("https://discord.gg/VuQjc8K");
+    }
+}
